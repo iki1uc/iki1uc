75c846671e64873f8f65dd753c05b5b390dc1491
iki1uc <entertrainer@web.de>
2026-06-01 01:45:31 +0200

    Update file4.js

--- file4.js
+++ file4.js
@@ -1,1 +1,10 @@
+// file4.js — Oberfläche / 4D
+
+export function pfeiler_4(x) {
+    return x !== undefined
+}
+
+export function anker_4(x) {
+    return { value: x, time: performance.now(), dim: "4D" }
+}
 
