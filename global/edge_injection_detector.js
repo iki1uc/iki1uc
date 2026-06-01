b84c1ca4dd61cca3b2a9411cc40a2584fe974391
iki1uc <entertrainer@web.de>
2026-06-01 15:20:09 +0200

    Update edge_injection_detector.js

--- global/edge_injection_detector.js
+++ global/edge_injection_detector.js
@@ -1,1 +1,20 @@
+import { ERROR_MONITOR } from "./error_monitor.js";
+
+const suspicious = [
+    "edge_all_open_tabs",
+    "WebsiteContent_",
+    "IsCurrent",
+    "pageUrl",
+    "pageTitle"
+];
+
+document.addEventListener("DOMContentLoaded", () => {
+    const html = document.documentElement.innerHTML;
+
+    suspicious.forEach(word => {
+        if(html.includes(word)){
+            ERROR_MONITOR.report("EDGE_INJECTION", "Browser hat Tab-Metadaten in die Datei geschrieben.");
+        }
+    });
+});
 
