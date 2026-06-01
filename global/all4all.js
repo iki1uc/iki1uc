b9fe32563061e39d8cbc4400cb6a6c891bd6c562
iki1uc <entertrainer@web.de>
2026-06-01 14:12:39 +0200

    Update all4all.js

--- global/all4all.js
+++ global/all4all.js
@@ -1,1 +1,30 @@
+export const GLOBAL_ALL4ALL = {
+    active: false,
+    anker: [],        // alle 128 Anker
+    watson: false,    // Watson-Signal
+
+    init(alleAnker){
+        this.anker = alleAnker;
+    },
+
+    on(){
+        this.active = true;
+        this.watson = true;
+
+        console.log("GLOBAL ALL4ALL → AKTIV");
+        console.log("WATSON → LEUCHTET");
+
+        this.anker.forEach(a => a.leuchten(true));
+    },
+
+    off(){
+        this.active = false;
+        this.watson = false;
+
+        console.log("GLOBAL ALL4ALL → AUS");
+        console.log("WATSON → DUNKEL");
+
+        this.anker.forEach(a => a.leuchten(false));
+    }
+};
 
