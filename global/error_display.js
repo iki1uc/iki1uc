f0f9739d97acc269bc886b30f6ad343f9525705b
iki1uc <entertrainer@web.de>
2026-06-01 15:19:28 +0200

    Update error_display.js

--- global/error_display.js
+++ global/error_display.js
@@ -1,1 +1,16 @@
+import { EVENTS } from "./events.js";
+
+EVENTS.on("system_error", err => {
+    const box = document.getElementById("systemErrorBox");
+    if(!box) return;
+
+    box.innerHTML = `
+        <div class="err">
+            <b>⚠ SYSTEMFEHLER</b><br>
+            <span>${err.type}</span><br>
+            <small>${err.detail}</small>
+        </div>
+    `;
+    box.style.display = "block";
+});
 
