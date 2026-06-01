5d0d9f73cc50d8c252f2d72019ae2eabfdd9a892
iki1uc <entertrainer@web.de>
2026-06-01 02:31:48 +0200

    Update core_halt.js

--- core/core_halt.js
+++ core/core_halt.js
@@ -1,1 +1,21 @@
+// ⭐ Zentrales halt() – für alle Seiten
+window.halt = function halt(eventName = "event", extra = {}) {
+  const lastEvent = {
+    clone: extra.clone || (window.PAGE_ID || "?"),
+    anchor: extra.anchor || (window.PAGE_ANCHOR || "PI"),
+    event: eventName,
+    time: Date.now(),
+    ...extra
+  };
+
+  if (window.NAVI && typeof NAVI.onEvent === "function") {
+    NAVI.onEvent(lastEvent);
+  }
+
+  if (window.BigCube && typeof BigCube.onEvent === "function") {
+    BigCube.onEvent(lastEvent);
+  }
+
+  return lastEvent;
+};
 
