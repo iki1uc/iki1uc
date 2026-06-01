6c4dc40510caf78aa5f5cf262fc61f7c7944329b
iki1uc <entertrainer@web.de>
2026-06-01 14:18:13 +0200

    Update techmap.js

--- global/techmap.js
+++ global/techmap.js
@@ -1,1 +1,24 @@
+export const TECHMAP = {
+    map: {},
+
+    // Technologie einem Anker zuordnen
+    register(techName, anker){
+        this.map[techName] = anker;
+    },
+
+    // Signal setzen
+    signal(techName, state){
+        const anker = this.map[techName];
+        if(!anker){
+            console.log("UNBEKANNT:", techName);
+            return;
+        }
+
+        anker.leuchten(state);
+
+        if(state){
+            console.log("ÜBELTÄTER:", techName);
+        }
+    }
+};
 
