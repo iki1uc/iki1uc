15772a8e98eebed4e80b2f31f17eae8c48762eea
iki1uc <entertrainer@web.de>
2026-06-06 00:48:12 +0200

    Update error_router.js

--- error_router.js
+++ error_router.js
@@ -1,1 +1,18 @@
+// error_router.js
+window.SEEU_ERROR_ROUTE = function(msg){
+  const r = window.SEEU_ERROR_INTERPRET(msg);
+
+  if (r.status === "NO_3X")  return { route:"RAW", info:r };
+  if (r.status === "DEFECT") return { route:"DEFECT", info:r };
+
+  switch (r.discipline){
+    case "MECH": return { route:"MECHANIK", info:r };
+    case "CMP":  return { route:"VERGLEICH", info:r };
+    case "TIME": return { route:"ZEIT", info:r };
+    case "META": return { route:"META", info:r };
+    case "OP":   return { route:"OPERATOR", info:r };
+    case "RT":   return { route:"REALTIME", info:r };
+    default:     return { route:"UNKNOWN", info:r };
+  }
+};
 
