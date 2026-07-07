8be3de897cfdf00d14a58b661f8c40c38117231e
iki1uc <entertrainer@web.de>
2026-07-08 00:41:10 +0200

    Update respo.js

--- respo.js
+++ respo.js
@@ -1,1 +1,17 @@
+const RESPO_AXIS = [
+  "SYS", "TEM", "IKI", "BIR",
+  "N0", "NS", "NC", "NC.sub",
+  "ART.link", "Ghost-Scan",
+  "TriAxiom", "BOERSE"
+];
+
+// Ghost-Scan Hook
+function ghostScanCarry() {
+  RESPO_AXIS.forEach((mod, i) => {
+    console.log(`Ghost-Scan trägt: ${mod} → RESPO-LAYER-${i}`);
+  });
+}
+
+// Startet die spätere Auswertung
+ghostScanCarry();
 
