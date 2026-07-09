fef4ddbd9f1516379cff79b18746976793a1c1b0
iki1uc <entertrainer@web.de>
2026-07-09 21:45:29 +0200

    Update bios_loader.js

--- BIOS/bios_loader.js
+++ BIOS/bios_loader.js
@@ -1,1 +1,26 @@
+function biosBoot() {
+    const out = document.getElementById("bios_out");
+
+    out.textContent =
+`[BOOT]
+Kernel geladen...
+Stationen prüfen...
+NC ✓
+Phi ✓
+Phi2 ✓
+PhiInfinity ✓
+
+BIOS: System erfolgreich gestartet.`;
+}
+
+function biosInfo() {
+    const out = document.getElementById("bios_out");
+
+    out.textContent =
+`[INFO]
+36E BIOS Version: 1.0
+Kernel: aktiv
+Stationen: 4
+Status: stabil`;
+}
 
