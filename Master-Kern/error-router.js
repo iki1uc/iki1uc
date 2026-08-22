ff99ecdf6aa34908e899027c7970d8e95aa111e0
iki1uc <entertrainer@web.de>
2026-08-22 11:58:13 +0200

    Update error-router.js

--- Master-Kern/error-router.js
+++ Master-Kern/error-router.js
@@ -8,17 +8,43 @@ document.addEventListener("DOMContentLoaded", () => {
 
       const url = link.getAttribute("href");
 
+      // MASTER-KERN: CACHE
+      const CACHE = {
+        load: 0,
+        max: 360,
+        add(x) {
+          this.load += x;
+          if (this.load > this.max) this.load = this.max;
+        },
+        status() {
+          if (this.load < 120) return "STABIL";
+          if (this.load < 360) return "HOCH";
+          return "VOLL";
+        }
+      };
+
+      // MASTER-KERN: ERROR-ROUTER
+      const ERROR_ROUTER = (path, reason) => {
+        const target = "./error.root.html?path=" + encodeURIComponent(path)
+                      + "&reason=" + encodeURIComponent(reason)
+                      + "&cache=" + CACHE.status();
+        window.location.href = target;
+      };
+
       try {
         const res = await fetch(url);
 
+        // Cache wächst bei jedem Zugriff
+        CACHE.add(42);
+
         if (!res.ok) {
-          window.location.href = "./error.root.html?path=" + url;
+          ERROR_ROUTER(url, "fetch-failed");
         } else {
           window.location.href = url;
         }
 
       } catch (e) {
-        window.location.href = "./error.root.html?path=" + url;
+        ERROR_ROUTER(url, "exception");
       }
     });
   });
