4693c4bfe5884a31aae6ae3818fea497046ae7c1
iki1uc <entertrainer@web.de>
2026-08-28 06:47:46 +0200

    Create pos.js

---
+++ sys/NCROM/pos/pos.js
@@ -0,0 +1,22 @@
+export const POS = {
+
+    mode: "ULTRA-U",
+    depth: {
+
+        // Essenz-Daten
+        data: "essenz",
+        mass: "essenz",
+
+        // Goldener Schnitt 6D
+        phi6: 0.5393446629,
+
+        // NC-Fraktal
+        fractal: NC.fractal,
+
+        // Drift
+        drift: NC.drift,
+
+        // Orbit-Tiles
+        orbit_tiles: ORBIT.paths.length
+    }
+};
