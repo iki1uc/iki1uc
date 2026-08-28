8b761ae3a38487746e624083da5e650be99fb9f0
iki1uc <entertrainer@web.de>
2026-08-28 09:06:45 +0200

    Update pipeline12.master.js

--- RAW/HW/GPU/9/pipeline12.master.js
+++ RAW/HW/GPU/9/pipeline12.master.js
@@ -1,72 +1,49 @@
-// RAW/HW/GPU/9/pipeline12.master.js
+stage1_buildMatrix() → ULTRA_KERNEL.build()
+stage2_respoBridge() → respo9hoch9()
+stage3_gpuStages() → GPU_MAP → Cubik4D.GPU
+stage4_output() → Cubik4D
+this.lastRespo = respo9hoch9(center.val, center.qi, center.iqq, center.octa);
+this.lastMovement = this.lastRespo.movement;
+this.lastGPU = this.lastRespo.gpuTensor;
+this.lastFusion = this.lastRespo.fusion;
+this.lastDirectV = this.lastRespo.directV;
+cubik.GPU = { stage1, stage2, stage3, stage4, level, mode, state };
+cubik.GPU692 = this.lastGPU;
+cubik.DirectV = this.lastDirectV;
+cubik.Fusion9 = this.lastFusion;
+cubik.Movement = this.lastMovement;
+return Cubik4D;
+stage2_respoBridge(){
+    const m = this.lastMatrix;
+    const center = m[4][4];
 
-import { ULTRA_KERNEL } from "./ultra.kernel.js";
-import { respo9hoch9 } from "./respo_bridge.js";
-import { Cubik4D } from "./cubik4d.js";
-import GPU_MAP from "../gpu.map";
+    this.lastRespo = respo9hoch9(center.val, center.qi, center.iqq, center.octa);
 
-export class PIPELINE12 {
+    this.lastMovement = this.lastRespo.movement;
+    this.lastGPU = this.lastRespo.gpuTensor;
+    this.lastFusion = this.lastRespo.fusion;
+    this.lastDirectV = this.lastRespo.directV;
 
-    constructor(){
-        this.kernel = window.ULTRA_KERNEL;
-        this.gpu = GPU_MAP;
-        this.lastMatrix = null;
-        this.lastRespo = null;
-        this.lastCubik = null;
-    }
-
-    // 1. ULTRA-KERNEL → 9×9 Matrix
-    stage1_buildMatrix(){
-        this.lastMatrix = this.kernel.build();
-        return this.lastMatrix;
-    }
-
-    // 2. respo_bridge → Achsen/Lage/Ghost → tmp + Cubik4D
-    stage2_respoBridge(){
-        const m = this.lastMatrix;
-        const center = m[4][4];
-
-        this.lastRespo = respo9hoch9(
-            center.val,
-            center.qi,
-            center.iqq,
-            center.octa
-        );
-
-        return this.lastRespo;
-    }
-
-    // 3. GPU-MAP → GPU-Stages in den Kubus einrechnen
-    stage3_gpuStages(){
-        const cubik = Cubik4D;
-
-        cubik.GPU = {
-            stage1: this.gpu.STAGE?.["1"],
-            stage2: this.gpu.STAGE?.["2"],
-            stage3: this.gpu.STAGE?.["3"],
-            stage4: this.gpu.STAGE?.["4"],
-            level: this.gpu.LEVEL,
-            mode: this.gpu.MODE,
-            state: this.gpu.STATE
-        };
-
-        return cubik.GPU;
-    }
-
-    // 4. Finaler 5D-Kubus zurückgeben
-    stage4_output(){
-        this.lastCubik = Cubik4D;
-        return this.lastCubik;
-    }
-
-    // MASTER-PIPELINE
-    start(){
-        this.stage1_buildMatrix();
-        this.stage2_respoBridge();
-        this.stage3_gpuStages();
-        return this.stage4_output();
-    }
+    return this.lastRespo;
 }
 
-window.PIPELINE12 = new PIPELINE12();
+stage3_gpuStages(){
+    const cubik = Cubik4D;
 
+    cubik.GPU = {
+        stage1: this.gpu.STAGE?.["1"],
+        stage2: this.gpu.STAGE?.["2"],
+        stage3: this.gpu.STAGE?.["3"],
+        stage4: this.gpu.STAGE?.["4"],
+        level: this.gpu.LEVEL,
+        mode: this.gpu.MODE,
+        state: this.gpu.STATE
+    };
+
+    cubik.GPU692 = this.lastGPU;
+    cubik.DirectV = this.lastDirectV;
+    cubik.Fusion9 = this.lastFusion;
+    cubik.Movement = this.lastMovement;
+
+    return cubik;
+}
