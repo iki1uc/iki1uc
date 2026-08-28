f11681eb2f7334f4f4c84e0e6f87bfe67ea3e61a
iki1uc <entertrainer@web.de>
2026-08-28 08:20:11 +0200

    Update tmp.js

--- RAW/HW/GPU/9/tmp.js
+++ RAW/HW/GPU/9/tmp.js
@@ -1,16 +1,61 @@
-// 9hoch9 / tmp.js
+import { updateTmp } from "./tmp.js";
+import { syncTmpToCubik } from "./cubik4d-sync.js";
 
-export const tmp = {
-    lastPosition: null,
-    lastAxes: null,
-    lastMovement: null,
-    lastOrbit: null
-};
+class ULTRA_KERNEL {
 
-export function updateTmp(position, axes, movement, orbit) {
-    tmp.lastPosition = position;
-    tmp.lastAxes = axes;
-    tmp.lastMovement = movement;
-    tmp.lastOrbit = orbit;
+    build(){
+        const raw = window.NC9X9 || [];
+        const out = [];
+
+        for(let r=0; r<9; r++){
+            const row = [];
+            for(let c=0; c<9; c++){
+                const val = raw[r][c];
+
+                row.push({
+                    r, c,
+                    val,
+                    qi: AXES.qi(r,c),
+                    iqq: AXES.iqq(r,c),
+                    octa: AXES.octa(r,c),
+                    orbit: GHOST.orbit(r,c),
+                    lage: LAGE.pos(r,c),
+                    core: XCORE.map(r,c),
+                    in: XIN.map(r,c),
+                    out: XOUT.map(r,c)
+                });
+            }
+            out.push(row);
+        }
+
+        return out;
+    }
+
+    sendToVector(matrix){
+        VECTOR?.receive9hoch9(matrix);
+    }
+
+    sendToALL(matrix){
+        ALL?.receive9hoch9(matrix);
+    }
+
+    sendToCubik(matrix){
+        const pos = matrix[4][4];        // Zentrum
+        const axes = AXES.fullMatrix();  // komplette Achsen
+        const movement = FLOW.vector();  // Bewegungsvektor
+        const orbit = ORBIT.state();     // Umlaufzustand
+
+        updateTmp(pos, axes, movement, orbit);
+        syncTmpToCubik();
+    }
+
+    start(){
+        const matrix = this.build();
+        this.sendToVector(matrix);
+        this.sendToALL(matrix);
+        this.sendToCubik(matrix);
+        return matrix;
+    }
 }
 
+window.ULTRA_KERNEL = new ULTRA_KERNEL();
