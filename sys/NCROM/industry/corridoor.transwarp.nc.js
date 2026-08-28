aea41d89d5ffea081df1640514c2af67cf2195f9
iki1uc <entertrainer@web.de>
2026-08-28 06:29:07 +0200

    Update corridoor.transwarp.nc.js

--- sys/NCROM/industry/corridoor.transwarp.nc.js
+++ sys/NCROM/industry/corridoor.transwarp.nc.js
@@ -1,8 +1,13 @@
-Ableitung:
-3e = XyX + AIR + AIV
-3e → 3³ = 27
-3³ → 9³ = 729
-9³ → 81
+NCROM.ABLEITUNG = {
+  "3e": ["XyX", "AIR", "AIV"],
 
-NC.feld = 81 / 3e
+  STUFEN: {
+    "3e → 3³": 27,
+    "3³ → 9³": 729,
+    "9³ → 81": 81
+  },
 
+  NC_feld: "81 / 3e",
+  SINGULAR: "golden wloch",
+  GATE: "allgateall"
+};
